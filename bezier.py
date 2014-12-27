1424db33c9c3f711093fb18881c4aeb75bc86f13
Adric Worley <adric.worley@epicgames.com>
2014-12-27 18:54:15 -0500

    Added Wu AA drawing to Bezier. Looks bad with non-integer end-points.

--- bezier.py
+++ bezier.py
@@ -1,11 +1,13 @@
 from PIL import Image, ImageDraw
 from collections import namedtuple
+import wu_aa as wu
 
 Point = namedtuple('Point', ['x', 'y'])
 
 wd = 500
 ht = 500
 im = Image.new('RGB', (wd, ht))
+
 
 def lerp(p1, p2, scale):
     """
@@ -20,8 +22,8 @@ def lerp(p1, p2, scale):
 
 draw = ImageDraw.Draw(im)
 
-p1 = Point(0,0)
-p2 = Point(wd-1, ht-1)
+pt1 = Point(0, 0)
+pt2 = Point(wd-1, ht-1)
 c1 = Point(wd*2, 0)
 c2 = Point(-wd, ht)
 
@@ -29,15 +31,17 @@ points = []
 
 for w in range(0, wd):
     scl = float(w) / wd
-    a1 = lerp(p1, c1, scl)
+    a1 = lerp(pt1, c1, scl)
     a2 = lerp(c1, c2, scl)
-    a3 = lerp(c2, p2, scl)
+    a3 = lerp(c2, pt2, scl)
 
     b1 = lerp(a1, a2, scl)
     b2 = lerp(a2, a3, scl)
 
     points.append(lerp(b1, b2, scl))
 
-draw.line(points, fill=(255,0,0), width=1)
-
+for idx, pt in enumerate(points):
+    if idx < len(points) - 1:
+        pt1 = points[idx + 1]
+        wu.draw_line_aa(pt.x, pt.y, pt1.x, pt1.y, draw)
 im.save("bezier.png")
